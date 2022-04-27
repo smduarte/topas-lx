17b59743c95a5ef1d0942645d7fec40ef423bcdb
smduarte <smd@fct.unl.pt>
2022-04-27 23:37:30 +0100

    Create user-session.sh

---
+++ user-session.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p $HOME/.local/share/applications 
+mkdir -p $HOME/.config/pcmanfm/LXDE && cp /tmp/desktop-items-0.conf $HOME/.config/pcmanfm/LXDE/ 
+rm -f $HOME/.local/share/applications/*.desktop && cp /tmp/*.desktop  $HOME/.local/share/applications/
+
+export HOME=/home/topas
+export USER=topas
+
+/usr/bin/lxpanel --profile LXDE &
+/usr/bin/pcmanfm --desktop --profile LXDE &
+
+exec /usr/bin/openbox
