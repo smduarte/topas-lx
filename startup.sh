cbb722c93da67366a5e5b1cf4ed1b7dc4b058f18
smduarte <smd@fct.unl.pt>
2022-04-27 22:55:21 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,6 +7,7 @@ sudo iptables -A OUTPUT -d $MOOSHAK -j ACCEPT
 sudo iptables -A OUTPUT -d 127.0.0.1/8 -j ACCEPT
 sudo iptables -A OUTPUT -j REJECT
 
+sudo deluser topas sudo
 sudo rm -f /etc/sudoers.d/topas
 
 mkdir -p $HOME/.local/share/applications 
