f5b7ea76aa550838e556da2a1467d44c3a2d3881
smduarte <smd@fct.unl.pt>
2022-04-27 23:38:41 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,19 +7,7 @@ sudo iptables -A OUTPUT -d $MOOSHAK -j ACCEPT
 sudo iptables -A OUTPUT -d 127.0.0.1/8 -j ACCEPT
 sudo iptables -A OUTPUT -j REJECT
 
-sudo deluser topas sudo
-sudo rm -f /etc/sudoers.d/topas
+#sudo deluser topas sudo
+#sudo rm -f /etc/sudoers.d/topas
 
-mkdir -p $HOME/.local/share/applications 
-mkdir -p $HOME/.config/pcmanfm/LXDE && cp /tmp/desktop-items-0.conf $HOME/.config/pcmanfm/LXDE/ 
-rm -f $HOME/.local/share/applications/*.desktop && cp /tmp/*.desktop  $HOME/.local/share/applications/
-
-export HOME=/home/topas
-export USER=topas
-
-su topas -c /usr/bin/lxpanel --profile LXDE &
-su topas -c /usr/bin/pcmanfm --desktop --profile LXDE &
-
-#exec /bin/tini -s -- /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
-
-exec su topas -c /usr/bin/openbox
+exec su topas -c /user-session.h
