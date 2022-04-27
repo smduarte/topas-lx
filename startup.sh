70316ac783e4926386670b87d59793a24df45dca
smduarte <smd@fct.unl.pt>
2022-04-28 00:19:32 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,12 +3,9 @@ echo "Starting..."
 
 MOOSHAK=192.168.100.1
 
-sudo iptables -A OUTPUT -d $MOOSHAK -j ACCEPT
-sudo iptables -A OUTPUT -d 127.0.0.1/8 -j ACCEPT
-sudo iptables -A OUTPUT -j REJECT
-
-#sudo deluser topas sudo
-#sudo rm -f /etc/sudoers.d/topas
+#sudo iptables -A OUTPUT -d $MOOSHAK -j ACCEPT
+#sudo iptables -A OUTPUT -d 127.0.0.1/8 -j ACCEPT
+#sudo iptables -A OUTPUT -j REJECT
 
 xhost +
 export DISPLAY=:0
