aa78edc9b505b040e01203e00613df5a2bcfd61a
smduarte <smd@fct.unl.pt>
2022-04-27 22:53:23 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,6 +7,7 @@ sudo iptables -A OUTPUT -d $MOOSHAK -j ACCEPT
 sudo iptables -A OUTPUT -d 127.0.0.1/8 -j ACCEPT
 sudo iptables -A OUTPUT -j REJECT
 
+sudo rm -f /etc/sudoers.d/topas
 
 mkdir -p $HOME/.local/share/applications 
 mkdir -p $HOME/.config/pcmanfm/LXDE && cp /tmp/desktop-items-0.conf $HOME/.config/pcmanfm/LXDE/ 
@@ -14,8 +15,6 @@ rm -f $HOME/.local/share/applications/*.desktop && cp /tmp/*.desktop  $HOME/.loc
 
 export HOME=/home/topas
 export USER=topas
-
-sudo deluser $USER sudo
 
 /usr/bin/lxpanel --profile LXDE &
 /usr/bin/pcmanfm --desktop --profile LXDE &
