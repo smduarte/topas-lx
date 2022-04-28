5b94168b43a235d34496568e32e55960e6c9f646
smduarte <smd@fct.unl.pt>
2022-04-28 22:03:57 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,9 +3,9 @@ echo "Starting..."
 
 MOOSHAK=10.170.138.23
 
-sudo iptables -A OUTPUT -d $MOOSHAK -j ACCEPT
-sudo iptables -A OUTPUT -d 127.0.0.1/8 -j ACCEPT
-sudo iptables -A OUTPUT -j REJECT
+sudo iptables -A OUTPUT -p tcp -d $MOOSHAK -j ACCEPT
+sudo iptables -A OUTPUT -p tcp -d 127.0.0.1/8 -j ACCEPT
+sudo iptables -A OUTPUT -p tcp -j REJECT
 
 xhost +
 export DISPLAY=:0
