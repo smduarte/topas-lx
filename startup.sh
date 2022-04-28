81abe34632fdcf82884460c53cecac4414453969
smduarte <smd@fct.unl.pt>
2022-04-28 09:15:07 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Starting..."
 
-MOOSHAK=192.168.100.1
+MOOSHAK=10.170.138.23
 
-#sudo iptables -A OUTPUT -d $MOOSHAK -j ACCEPT
-#sudo iptables -A OUTPUT -d 127.0.0.1/8 -j ACCEPT
-#sudo iptables -A OUTPUT -j REJECT
+sudo iptables -A OUTPUT -d $MOOSHAK -j ACCEPT
+sudo iptables -A OUTPUT -d 127.0.0.1/8 -j ACCEPT
+sudo iptables -A OUTPUT -j REJECT
 
 xhost +
 export DISPLAY=:0
