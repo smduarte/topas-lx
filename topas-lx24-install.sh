2667e59229319ab18f2b0da4d03d85831def34e1
smduarte <smd@fct.unl.pt>
2024-05-09 10:55:54 +0100

    install script

---
+++ topas-lx24-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -O https://raw.githubusercontent.com/smduarte/topas-lx/main/Xmodmap > .Xmodmap
+wget -O https://raw.githubusercontent.com/smduarte/topas-lx/main/dconf-keyboard-bindings.dconf | dconf load
+
+wget -O vm-topas-lx24.sh https://raw.githubusercontent.com/smduarte/topas-lx/main/vm-topas-lx24.sh
+
+echo "Logout so changes take effect!!!!"
