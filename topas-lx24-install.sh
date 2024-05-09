9a9a95d6ebcdd25b0127da97bdea756ba7834ffd
smduarte <smd@fct.unl.pt>
2024-05-09 11:02:07 +0100

    topas

--- topas-lx24-install.sh
+++ topas-lx24-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+gsettings set org.gnome.desktop.wm.keybindings panel-main-menu "[]" # disable Alt+F1
 
 wget -O https://raw.githubusercontent.com/smduarte/topas-lx/main/Xmodmap > .Xmodmap
 wget -O https://raw.githubusercontent.com/smduarte/topas-lx/main/dconf-keyboard-bindings.dconf | dconf load
