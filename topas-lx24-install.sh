efb90e31621e0cf0e660a5adb5c0426ced7d7c88
smduarte <smd@fct.unl.pt>
2024-05-10 01:03:02 +0100

    Update topas-lx24-install.sh

--- topas-lx24-install.sh
+++ topas-lx24-install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+gsettings set org.gnome.shell.extensions.apps-menu apps-menu-toggle-menu "[]"
 
 wget -O - https://raw.githubusercontent.com/smduarte/topas-lx/main/user > .config/dconf/user
+
+wget https://raw.githubusercontent.com/smduarte/topas-lx/main/user
+
 wget -O - https://raw.githubusercontent.com/smduarte/topas-lx/main/Xmodmap > .Xmodmap
 wget -O - https://raw.githubusercontent.com/smduarte/topas-lx/main/dconf-keyboard-bindings.dconf | dconf load /
 
